9337c3530f3df340932019d88f7d538ccc96b4c3
abdul <abdul.tanveer@gmail.com>
2024-02-26 17:04:55 +0530

    connection and  db creation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,32 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String url = "jdbc:postgresql://localhost:5432/target";
+        String username = "user";
+        String  password = "";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try {
+            Connection connection = DriverManager.getConnection(url,username,password);
+            Statement databaseCreation = connection.createStatement();
+           // databaseCreation.executeUpdate("DROP DATABASE IF EXISTS JDBC_DB");
+            //databaseCreation.executeUpdate("CREATE DATABASE TARGET");
+           // connection = DriverManager.getConnection(url+"target");
+            StudentDAO studentDAO = new StudentDAO(connection);
+            Student myStudent = new Student(12,"abdul","ansari","emailabdul","9999","addrs","blr","kar","56000");
+            studentDAO.create(myStudent);
+
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+
     }
 }
