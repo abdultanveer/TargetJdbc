cedbb53f1534b04cc85884971964b7a2b2d4012b
abdul <abdul.tanveer@gmail.com>
2024-02-26 17:26:40 +0530

    drop db if it exists

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,23 +9,28 @@ import java.sql.Statement;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://localhost:5432/target";
+        String url = "jdbc:postgresql://localhost:5432/";
+        String url1 = "jdbc:postgresql://localhost:5432/target";
+
         String username = "user";
         String  password = "";
 
         try {
             Connection connection = DriverManager.getConnection(url,username,password);
             Statement databaseCreation = connection.createStatement();
-           // databaseCreation.executeUpdate("DROP DATABASE IF EXISTS JDBC_DB");
-            //databaseCreation.executeUpdate("CREATE DATABASE TARGET");
-           // connection = DriverManager.getConnection(url+"target");
-            StudentDAO studentDAO = new StudentDAO(connection);
-            Student myStudent = new Student(12,"abdul","ansari","emailabdul","9999","addrs","blr","kar","56000");
+            //SELECT 'CREATE DATABASE <db_name>'
+            //WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '<db_name>')
+            databaseCreation.executeUpdate("DROP DATABASE IF EXISTS JDBC_DB");
+            databaseCreation.executeUpdate("CREATE DATABASE TARGET");
+
+            Connection dbconnection = DriverManager.getConnection(url+"target");
+            StudentDAO studentDAO = new StudentDAO(dbconnection);
+            Student myStudent = new Student(16,"abdul1","ansari1","emailabdul","9999","addrs","blr","kar","56000");
             studentDAO.create(myStudent);
 
             connection.close();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+
         }
 
     }
